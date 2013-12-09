cde23b0c3547589c9b98d28713168615e2a6bd8b
Pan GaoYong <pan.gaoyong@gmail.com>
2013-12-09 15:54:01 +0800

    A rails 4 application my vocabulary

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Myvoc::Application.config.secret_key_base = '88f944786ae55e93178411fc49a46a77d373f98bad31a45a073ae934c62e8ae648bd69e67f39778c390106c7551a038668e2dff97a66eb9416152f571d87113b'
