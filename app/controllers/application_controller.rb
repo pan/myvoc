c993701f076d3f32a25c1336fcf27ddb4e7edff1
Pan GaoYong <pan.gaoyong@gmail.com>
2017-04-14 22:51:55 +0800

    add a few common methods used by controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,24 @@
+# frozen_string_literal: true
+
+# application controller is the base for other controllers
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  # check authentication status, protect from unauthorized calls to 
+  delegate :login?, to: :helpers
+
+  # check authentication status, protect from unauthorized calls to
   # administrative methods
   def authenticate
-    unless session[:user_id]
-      render text: "login required." 
-    end
+    render text: 'login required.' unless login?
   end
 
+  def uid
+    session[:user_id]
+  end
+
+  def current_user
+    User.i(uid)
+  end
 end
