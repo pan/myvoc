b1f4050ddca3b46bb3e81d35dcd81a07e400e7cd
Pan GaoYong <pan.gaoyong@gmail.com>
2017-04-14 22:44:56 +0800

    rubocop users helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
+# users helper module
 module UsersHelper
   # return true if a user has logged in
   def login?
-    session[:user_id] ? true : false
+    !session[:user_id].nil?
   end
 end
