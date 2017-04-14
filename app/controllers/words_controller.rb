b378aa37a2dba0c072f6baeeaddafcea7ff43412
Pan GaoYong <pan.gaoyong@gmail.com>
2017-04-14 22:38:47 +0800

    prepare for new version of camdict

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,58 +1,62 @@
+# frozen_string_literal: true
+
+# the main controller of this app
 class WordsController < ApplicationController
-  before_action :set_word, only: [:show, :destroy]
-  before_action :authenticate, only: [:create, :destroy]
-  before_action :search_word, only: [:index, :suggested]
+  before_action :set_word, only: %i[show destroy]
+  before_action :authenticate, only: %i[create destroy]
+  before_action :search_word, only: %i[index suggested]
   respond_to :html, :json
 
   def index
     @words = @words.desc(:updated_at).page params[:page]
-    @matched = (@words.where word: params[:term]).exists?
-    session[:word] = params[:term] if @matched
-    @definitions = session[:word] ? Word.get_defs(session[:word]) : rand_defs
-    @word_count = Word.count_words session[:user_id]
+    set_session_word
+    @word = Word.i(session[:word]) || rand_word
+    @word_count = Word.count_words uid
   end
 
   def show
-    @word = Word.find(params[:id])
     session[:word] = @word.word
-    @definitions = @word.definitions
-    render partial: 'definition', layout: false
+    render partial: 'definition'
   end
 
   def create
     word = params[:term].squish
-    jid = CamdictWorker.perform_async session[:user_id], word
-    redirect_to words_path, notice: jid ? 
-      "Job(id:#{jid}) should be completed in a minute." : "Job not created."
+    if Word.add_asso(current_user, word)
+      session[:word] = word
+      msg = "#{word} added"
+    else
+      msg = "#{word} not found"
+    end
+    redirect_to words_path, notice: msg
   end
 
   def destroy
-    @words = User.find(session[:user_id]).words
+    @words = current_user.words
     @words.delete(@word)
-    render json: {id: @word.id.to_s}
+    render json: { id: @word.id.to_s }
   end
 
   def suggested
-    list = @words.map {|w| w.word }
+    list = @words.map(&:word)
     respond_with list
   end
 
   private
 
-  # set the words scope to those belonging to current user if logged in, 
+  def set_session_word
+    matched = (@words.where word: params[:term]).exists?
+    session[:word] = params[:term] if matched
+  end
+
+  # set the words scope to those belonging to current user if logged in,
   # otherwise the scope is all words in DB
   def search_word
-    if session[:user_id]
-      user = User.find(session[:user_id])
-      @words = user.words if user
-      if @words
-        @words = @words.search params[:term] 
+    @words =
+      if login? && current_user.words.exists?
+        current_user.words.search params[:term]
       else
-        @words = Word.search params[:term] 
+        Word.search params[:term]
       end
-    else
-      @words = Word.search params[:term]
-    end
   end
 
   def set_word
@@ -63,9 +67,9 @@ class WordsController < ApplicationController
     params.require(:word).permit(:word)
   end
 
-  # definitions of a random word
-  def rand_defs
+  # a random word
+  def rand_word
     idx = rand(@words.count)
-    @words[idx]&.definitions
+    @words[idx]
   end
 end
--- test/controllers/words_controller_test.rb
+++ test/controllers/words_controller_test.rb
@@ -1,39 +1,54 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class WordsControllerTest < ActionController::TestCase
-
-  test "should get index" do
+  test 'should get index' do
     get :index
     assert_response :success
     assert assigns(:words)
-    assert_select "title", "My Vocabulary"
-    assert_select "span", {text: "Logged off"}
+    assert_select 'title', 'My Vocabulary'
+    assert_select 'span', text: 'Logged off'
   end
 
-  test "should show word" do
+  test 'should show word' do
     moon = Word.find_by word: 'moon'
     get :show, params: { id: moon.id.to_s }
     assert_template partial: '_definition'
     assert_response :success
-    assert_select ".sentence", 5
+    assert_select '.sentence', 5
   end
 
-  test "should create word" do
+  test 'should create word' do
     login_tester
-    post :create, params: { term: 'automation' }
+    drop_if_exists('factory')
+    post :create, params: { term: 'factory' }
     assert_redirected_to words_path
+    drop_if_exists('factory')
   end
 
-  test "should delete word" do
+  test 'should delete word' do
     login_tester
-    word = Word.find_by word: 'automation'
-    delete :destroy, params: { id: word.id.to_s, format: 'json' }
+    post :create, params: { term: 'somewhat' }
+    word = Word.find_by word: 'somewhat'
+    delete :destroy, params: { id: word.id.to_s }, as: 'json'
+    drop_if_exists('somewhat')
     assert_response :success
   end
 
-  test "should suggest word" do
+  test 'should suggest word' do
     session[:user_id] = nil
-    get :suggested, params: { format: :json }
+    get :suggested, as: :json
     assert_response :success
+  end
+
+  private
+
+  def drop_if_exists(word)
+    w = Word.find_by word: word
+    User.all.each { |u| u.de_associate(w) }
+    w.destroy
+  rescue Mongoid::Errors::DocumentNotFound
+    false
   end
 end
