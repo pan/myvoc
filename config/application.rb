0fc2acb03bd748c7adb8f9f1d1dfcf7c94279f14
Pan GaoYong <pan.gaoyong@gmail.com>
2017-05-03 15:12:43 +0800

    rails db:update to 5.1

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ Bundler.require(*Rails.groups)
 module Myvoc
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
