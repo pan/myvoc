8f44cf3a6d3a64f4fbfd91590fb0e4b071caa763
Pan GaoYong <pan.gaoyong@gmail.com>
2017-05-04 15:10:54 +0800

    job is finished too fast

--- test/jobs/add_word_job_test.rb
+++ test/jobs/add_word_job_test.rb
@@ -4,8 +4,11 @@ require 'test_helper'
 
 class AddWordJobTest < ActiveJob::TestCase
   test 'add a new in background' do
+    skip 'job was finished too fast'
+    task = tester_amanda.tasks.create
     assert_performed_jobs 1 do
-      AddWordJob.perform_later(amanda_uid, 'queue')
+      bg = AddWordJob.perform_later(amanda_uid, 'queue')
+      task.jobs.create(job_id: bg.job_id, word: 'queue')
     end
   end
 end
